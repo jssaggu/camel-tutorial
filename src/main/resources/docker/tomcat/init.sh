7f9ce9b44280427770ebeeed5c61f72d571920ac
jasvinder-saggu <jasvinder.saggu@mastercard.com>
2022-03-27 21:59:17 +0100

    Added tomcat

---
+++ src/main/resources/docker/tomcat/init.sh
@@ -0,0 +1,3 @@
+mv /usr/local/tomcat/webapps /usr/local/tomcat/webapps2
+mv /usr/local/tomcat/webapps.dist /usr/local/tomcat/webapps
+cp /tmp/context.xml /usr/local/tomcat/webapps/manager/META-INF/context.xml
