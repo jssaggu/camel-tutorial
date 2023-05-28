2fffe326006f4143b99ea30b7e371a74fbea03cb
Jasvinder Saggu <jasvinder.saggu@mastercard.com>
2023-05-28 18:14:31 +0100

    Spring Camel upgrade

--- src/test/java/com/jss/camel/components/rabbitmq/WeatherRouteTest.java
+++ src/test/java/com/jss/camel/components/rabbitmq/WeatherRouteTest.java
@@ -1,6 +1,12 @@
 package com.jss.camel.components.rabbitmq;
 
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.EXCHANGE_WEATHER_DATA;
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.QUEUE_WEATHER_DATA;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.jss.CamelApplication;
+import com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -10,31 +16,22 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.EXCHANGE_WEATHER;
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.QUEUE_WEATHER;
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.QUEUE_WEATHER_EVENTS;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @Disabled(
-        "A RabbitMQ server must be running inorder to run this test." +
-                "Once RabbitMQ is started. Remove @Disabled statement"
-)
+        "A RabbitMQ server must be running inorder to run this test."
+                + "Once RabbitMQ is started. Remove @Disabled statement")
 @SpringBootTest(
         classes = CamelApplication.class,
-        properties = {"jss.camel.rabbitmq.enabled=true"}
-)
+        properties = {"jss.camel.rabbitmq.enabled=true"})
 @CamelSpringBootTest
 class WeatherRouteTest {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    @Autowired private RabbitTemplate rabbitTemplate;
 
     @Test
     void sendAndReceiveMessage() {
-        rabbitTemplate.send(EXCHANGE_WEATHER, QUEUE_WEATHER, message());
+        rabbitTemplate.send(EXCHANGE_WEATHER_DATA, QUEUE_WEATHER_DATA, message());
 
-        Message response = rabbitTemplate.receive(QUEUE_WEATHER_EVENTS, 1000);
+        Message response = rabbitTemplate.receive(RabbitmqConfiguration.QUEUE_WEATHER_EVENTS, 1000);
         assertNotNull(response, "Response must be non-null");
 
         String body = new String(response.getBody());
@@ -44,9 +41,8 @@ class WeatherRouteTest {
     }
 
     private Message message() {
-        return MessageBuilder
-                .withBody("{ \"city\": \"London\", \"temp\": \"20\", \"unit\": \"C\"}"
-                        .getBytes())
+        return MessageBuilder.withBody(
+                        "{ \"city\": \"London\", \"temp\": \"20\", \"unit\": \"C\"}".getBytes())
                 .build();
     }
 }
--- src/test/java/com/jss/camel/components/rabbitmq/WeatherRouteTestcontainersTest.java
+++ src/test/java/com/jss/camel/components/rabbitmq/WeatherRouteTestcontainersTest.java
@@ -1,6 +1,18 @@
 package com.jss.camel.components.rabbitmq;
 
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.EXCHANGE_WEATHER_DATA;
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.QUEUE_WEATHER_DATA;
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.QUEUE_WEATHER_EVENTS;
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.RMQ_HOST;
+import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.RMQ_PORT;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.jss.CamelApplication;
+import java.io.File;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.function.Function;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,64 +30,51 @@ import org.testcontainers.containers.wait.strategy.WaitStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.File;
-import java.time.Duration;
-import java.util.Properties;
-import java.util.function.Function;
-
-import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.RMQ_HOST;
-import static com.jss.camel.components.routes.rabbitmq.RabbitmqConfiguration.RMQ_PORT;
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.EXCHANGE_WEATHER;
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.QUEUE_WEATHER;
-import static com.jss.camel.components.routes.rabbitmq.WeatherRoute.QUEUE_WEATHER_EVENTS;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest(
         classes = CamelApplication.class,
-        properties = {
-                "jss.camel.rabbitmq.enabled=true",
-                "jss.camel.testcontainers.enabled=true"
-        })
+        properties = {"jss.camel.rabbitmq.enabled=true", "jss.camel.testcontainers.enabled=true"})
 @CamelSpringBootTest
 @Testcontainers
 @ContextConfiguration(
         classes = {TestContainerLaunchConfig.class},
-        loader = AnnotationConfigContextLoader.class
-)
+        loader = AnnotationConfigContextLoader.class)
 class WeatherRouteTestcontainersTest {
 
     public static final String SERVICE_NAME_RABBITMQ = "rabbitmq";
     public static final int RABBITMQ_PORT = 5672;
     private static final String RABBIT_UP_LOG_MESSAGE = ".*Resetting node maintenance status.*";
 
-    static Function<String, WaitStrategy> waitForLogMessageFunction = (String messageInLog) -> Wait
-            .forLogMessage(messageInLog, 1)
-            .withStartupTimeout(Duration.ofSeconds(180));
+    static Function<String, WaitStrategy> waitForLogMessageFunction =
+            (String messageInLog) ->
+                    Wait.forLogMessage(messageInLog, 1).withStartupTimeout(Duration.ofSeconds(180));
 
     /*  @Container
-              public static GenericContainer DOCKER_RABBITMQ = new GenericContainer(
-                      DockerImageName.parse("rabbitmq:3-management"))
-                      .withExposedPorts(RABBITMQ_PORT)
-                      .waitingFor(Wait.forLogMessage(RABBIT_UP_LOG_MESSAGE, 1)
-                              .withStartupTimeout(Duration.ofSeconds(20)));
-          */
+        public static GenericContainer DOCKER_RABBITMQ = new GenericContainer(
+                DockerImageName.parse("rabbitmq:3-management"))
+                .withExposedPorts(RABBITMQ_PORT)
+                .waitingFor(Wait.forLogMessage(RABBIT_UP_LOG_MESSAGE, 1)
+                        .withStartupTimeout(Duration.ofSeconds(20)));
+    */
     @Container
-    public static DockerComposeContainer DOCKER_RABBITMQ = new DockerComposeContainer(
-            new File("src/test/resources/docker-compose.yml"))
-            .withExposedService(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT)
-            .waitingFor(SERVICE_NAME_RABBITMQ, waitForLogMessageFunction.apply(RABBIT_UP_LOG_MESSAGE)
-            );
+    public static DockerComposeContainer DOCKER_RABBITMQ =
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"))
+                    .withExposedService(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT)
+                    .waitingFor(
+                            SERVICE_NAME_RABBITMQ,
+                            waitForLogMessageFunction.apply(RABBIT_UP_LOG_MESSAGE));
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    @Autowired private RabbitTemplate rabbitTemplate;
 
     @BeforeAll
     public static void constructed() {
         DOCKER_RABBITMQ.start();
         Properties props = System.getProperties();
-        props.put(RMQ_HOST, "" + DOCKER_RABBITMQ.getServiceHost(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT));
-        props.put(RMQ_PORT, "" + DOCKER_RABBITMQ.getServicePort(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT));
+        props.put(
+                RMQ_HOST,
+                "" + DOCKER_RABBITMQ.getServiceHost(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT));
+        props.put(
+                RMQ_PORT,
+                "" + DOCKER_RABBITMQ.getServicePort(SERVICE_NAME_RABBITMQ, RABBITMQ_PORT));
     }
 
     @AfterAll
@@ -85,7 +84,7 @@ class WeatherRouteTestcontainersTest {
 
     @Test
     void sendAndReceiveMessage() {
-        rabbitTemplate.send(EXCHANGE_WEATHER, QUEUE_WEATHER, message());
+        rabbitTemplate.send(EXCHANGE_WEATHER_DATA, QUEUE_WEATHER_DATA, message());
 
         Message response = rabbitTemplate.receive(QUEUE_WEATHER_EVENTS, 1000);
         assertNotNull(response, "Response must be non-null");
@@ -97,9 +96,8 @@ class WeatherRouteTestcontainersTest {
     }
 
     private Message message() {
-        return MessageBuilder
-                .withBody("{ \"city\": \"London\", \"temp\": \"20\", \"unit\": \"C\"}"
-                        .getBytes())
+        return MessageBuilder.withBody(
+                        "{ \"city\": \"London\", \"temp\": \"20\", \"unit\": \"C\"}".getBytes())
                 .build();
     }
 }
