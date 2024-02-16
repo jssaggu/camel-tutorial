72719caddd0b624491dfd223f525cbb7b6a7e1cb
jasvinder-saggu <jasvinder.saggu@mastercard.com>
2024-02-16 11:32:48 +0000

    Updating Spring and Camel version

--- src/main/java/com/jss/CamelApplication.java
+++ src/main/java/com/jss/CamelApplication.java
@@ -1,7 +1,5 @@
 package com.jss;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import org.apache.camel.opentelemetry.starter.CamelOpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +16,4 @@ public class CamelApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CamelApplication.class, args);
     }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {}
 }
